66cd220af63e20401f92ac43e3275e318d6186a0
BlendaHere <110112052+BlendaHere@users.noreply.github.com>
2023-06-09 23:50:47 +0200

    ndryshim i css

--- home/index.php
+++ home/index.php
@@ -1,6 +1,15 @@
 
     <title>ShoeShop</title>
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <style>
+    .swiper-slide {
+    flex-shrink: 0;
+    width: 70%;
+    height: 50%;
+    position: relative;
+    transition-property: transform;
+}
+</style>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
